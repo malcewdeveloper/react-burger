de578eef67d181a71b25a944140a9fca4f1fc53a
Maksim <malcewdeveloper@yandex.ru>
2024-11-23 23:15:48 +0300

    feat: добавляет компонент AppHeader

---
+++ src/components/app/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppHeader from "../app-header/AppHeader";
+
+function App() {
+    return (
+        <>
+            <AppHeader />
+        </>
+    );
+}
+
+export default App;
